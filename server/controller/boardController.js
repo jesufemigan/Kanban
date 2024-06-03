bc9585081b9a15fe43b01dccff3289173df95834
Jesufemi Oladapo <oladapofemmy@gmail.com>
2024-06-03 08:40:49 +0100

    board apis

--- server/controller/boardController.js
+++ server/controller/boardController.js
@@ -1,9 +1,33 @@
 import expressAsyncHandler from 'express-async-handler'
-const Board = {
-    'id': 1,
-    'name': "Design"
-}
+import Board from '../model/boardModel.js'
 
 export const getAllBoards = expressAsyncHandler(async (req, res) => {
-  res.status(200).json(Board)  
+    const boards = await Board.find()
+    res.status(200).json(boards)
+})
+
+export const addNewBoard = expressAsyncHandler(async (req, res) => {
+    const { title, columns } = req.body
+    const newBoard = new Board({
+        title,
+        columns
+    })
+    await newBoard.save()
+    res.json(newBoard)
+})
+
+export const editBoard = expressAsyncHandler(async (req, res) => {
+    const { board_id } = req.params
+    const { title, columns } = req.body
+
+    await Board.findByIdAndUpdate(board_id, { title, columns }, { new: true })
+    const allBoard = await Board.find()
+    res.status(200).json(allBoard)
+})
+
+export const deleteBoard = expressAsyncHandler(async (req, res) => {
+    const { board_id } = req.params
+    await Board.findByIdAndDelete(board_id)
+    const allBoard = await Board.find()
+    res.status(200).json(allBoard)
 })
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import boardRoutes from './routes/boardRoutes.js'
+import { connectDB } from './config/db.js'
 
+connectDB()
 const app = express()
-app.use('/api/v1/boards/', boardRoutes)
+app.use('/api/v1/boards', boardRoutes)
 
 app.listen(5000, () => console.log('App is running'))
